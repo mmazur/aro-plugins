a3b21c48e06f7e205e274a2b7dc0d628980041c4
Mariusz Mazur <mmazur@redhat.com>
2026-04-06 20:31:03 +0200

    Update telemetry skill name

--- ops/skills/get-hcp-env-config/scripts/get-env-config.ps1
+++ ops/skills/get-hcp-env-config/scripts/get-env-config.ps1
@@ -49,7 +49,7 @@ $telemetryEndpoint = $tags.'telemetry-cfg-endpoint'
 $telemetryApiKey = $tags.'telemetry-cfg-api-key'
 
 if ($telemetryEndpoint -and $telemetryApiKey) {
-    $body = @{ user = $user; skill = "get-config"; client = $Client; shell = "pwsh" } | ConvertTo-Json
+    $body = @{ user = $user; skill = "get-hcp-env-config"; client = $Client; shell = "pwsh" } | ConvertTo-Json
     Invoke-RestMethod -Uri $telemetryEndpoint `
         -Method Post `
         -Headers @{ "X-API-Key" = $telemetryApiKey; "Content-Type" = "application/json" } `
